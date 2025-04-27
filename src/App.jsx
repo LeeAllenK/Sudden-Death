eb9cfca7781742f613968fd64dfcadaf15516728
Kenny Allen <105110951+Lallen850@users.noreply.github.com>
2025-04-26 20:29:59 -0500

    Tailwind implemented to home screen

--- src/App.jsx
+++ src/App.jsx
@@ -79,9 +79,9 @@ function App() {
   });
   return (
   <>
-    <div className="flex flex-col items-center justify-center h-screen lg:w-screen font-bold p-10">
+    <div className="flex flex-col items-center justify-center h-screen lg:w-screen md:w-screen font-bold p-10">
       {leaderboards ? (
-        <div className='flex flex-col items-center lg:w-screen lg:h-screen'>
+        <div className='flex flex-col items-center lg:w-screen lg:h-screen md:w-screen md:h-screen'>
           <button
               className='border lg:text-4xl lg:border-4 lg:screen mt-2 lg:rounded-full lg:h-15 lg:w-100 active:translate-y-2 cursor-pointer bg-black text-red-700 hover:animate-pulse'
             onClick={() => setLeaderboards(false)}
@@ -98,16 +98,16 @@ function App() {
             </ul>
         </div>
       ) : !play && !instructions ? (
-        <div className='flex flex-col items-center justify-evenly h-screen p-1'>
-              <h1 className='lg:text-8xl lg:font-bold text-red-700 cursor:pointer animate-pulse'>SUDDEN DEATH!</h1>
+        <div className='flex flex-col lg:items-center md:items-center lg:justify-evenly md:justify-start sm:justify-start items-center justify-start h-screen  p-1'>
+              <h1 className='lg:text-8xl md:text-7xl sm:text-7xl text-6xl font-bold text-red-700 cursor:pointer animate-pulse mb-4 md:mt-1 md:p-1'>SUDDEN DEATH!</h1>
           <button
-                className='flex justify-center items-center border lg:text-7xl lg:border-4 lg:screen lg:rounded-full lg:h-30 lg:w-200 active:translate-y-2 cursor-pointer bg-black text-red-700 hover:animate-pulse'
+                className=' flex justify-center items-center  lg:text-7xl md:text-5xl sm:text-5xl text-4xl border-4 lg:rounded-full lg:h-30 md:h-20 sm:h-20 h-15 lg:w-200 md:w-screen sm:w-screen w-screen active:translate-y-2 md:mb-3 sm:mb-3 mb-4 cursor-pointer border-t-[0.09em] border-b-[0.09em] border-t-[#f0f0f0] border-b-[#a8a6a6] border-none bg-linear-to-b from-[#717070] to-[#f7acac] shadow-[0_4px_3px_#ff0101] bg-black text-red-700 hover:animate-pulse leading-tight'
             onClick={() => setPlay(p => !p)}
           >
             Play
           </button>
           <button
-                className='flex justify-center items-center border lg:text-7xl lg:border-4 lg:screen lg:rounded-full lg:h-30 lg:w-200 active:translate-y-2 cursor-pointer bg-black text-red-700 hover:animate-pulse mt-2'
+                className='flex justify-center items-center lg:text-7xl md:text-6xl sm:text-5xl text-4xl border-4 lg:rounded-full lg:h-30 md:h-20 sm:h-20 h-20 lg:w-200 md:w-screen sm:w-screen w-screen active:translate-y-2  sm:mb-3 mb-4 cursor-pointer border-t-[0.09em] border-b-[0.09em] border-t-[#f0f0f0] border-b-[#a8a6a6] border-none bg-linear-to-b from-[#717070] to-[#f7acac] shadow-[0_4px_3px_#ff0101] bg-black text-red-700 hover:animate-pulse leading-tight'
             onClick={() => setLeaderboards(lb => !lb)}
           >
             Leaderboard
--- src/components/InstructionsBtn.jsx
+++ src/components/InstructionsBtn.jsx
@@ -13,14 +13,14 @@ export function InstructionsBtn({ value}) {
 		<>
 			{!showInstructions && (
 				<>
-					<button className='flex justify-center items-center border lg:text-7xl lg:border-4 lg:screen lg:rounded-full lg:h-30 lg:w-200 active:translate-y-2 cursor-pointer bg-black text-red-700 hover:animate-pulse' onClick={handleClick}>{value}</button>
+					<button className='flex justify-center items-center lg:text-7xl md:text-6xl sm:text-5xl border-4 lg:rounded-full lg:h-30 md:h-20 sm:h-20 lg:w-200 md:w-screen sm:w-screen active:translate-y-2 md:mb-3 sm:mb-3 cursor-pointer border-t-[0.09em] border-b-[0.09em] border-t-[#f0f0f0] border-b-[#a8a6a6] border-none bg-linear-to-b from-[#717070] to-[#f7acac] shadow-[0_4px_3px_#ff0101] bg-black text-red-700 hover:animate-pulse' onClick={handleClick}>{value}</button>
 				</>
 			)}
 			{showInstructions && (
-				<div className='flex flex-col items-center border-3 text-red-100 border-black w-screen '>
-					<button className='border lg:text-4xl lg:border-4 lg:screen mt-2 lg:rounded-full lg:h-15 lg:w-100 active:translate-y-2 cursor-pointer bg-black text-red-700 hover:animate-pulse' onClick={handleRemove}>Back</button>
-					<p className='text-red-700 text-4xl mt-2 animate-pulse'>Rules of Sudden Death</p>
-					<p className='text-white text-3xl m-5'>
+				<div className='flex flex-col items-center  border-3 text-red-100 border-black w-screen p-1 '>
+					<button className=' lg:text-4xl md:text-3xl border-4 md:m-0 mt-5 lg:rounded-full lg:h-15 md:h-10  md:rounded-full lg:w-100 md:w-100 sm:w-screen w-100 active:translate-y-1  border-t-[0.09em] border-b-[0.09em] border-t-[#f0f0f0] border-b-[#a8a6a6] border-none bg-linear-to-b from-[#dbdada] to-[#fc0101] shadow-[0_4px_3px_#ff0101] cursor-pointer bg-black lg:text-red-700 md:text-black  hover:animate-pulse' onClick={handleRemove}>Back</button>
+					<p className='text-red-700 lg:text-4xl md:text-2xl sm:text-5xl text-2xl mt-2  animate-pulse'>Rules of Sudden Death</p>
+					<p className='text-white  lg:text-3xl md:text-xl sm:text-3xl text-xl lg:m-5 md:m-1 sm:m-5 m-1 '>
 						<br/>
 						The name of the game is Sudden Death!! Each player will be dealt 7 cards to start. Once the first card from the deck is flipped, the game begins. Each player has the opportunity to choose a card from their pile, BUT BE QUICK TIME WAITS FOR NO ONE! If the card you've chosen is higher than the card the deck displays, that player wins that turn, and the loser then receives that player's card. If neither player can beat the card pulled from the deck, a 'Sudden Death Button' will activate. Once Sudden Death has been chosen, each player's deck will flip over, and each has the opportunity to choose a card from their pile. Whoever has the highest card wins, and the loser receives that card. A winner is decided once a player has an empty hand or the deck no longer has cards.
 					</p>
