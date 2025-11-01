c61ad1f160c97659d9f3112365590a817d443712
Kenny Allen <105110951+Lallen850@users.noreply.github.com>
2025-11-01 13:41:28 -0500

    mobile issue

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'dist')));
-app.use(cors({origin: true}));
+app.use(cors({origin: 'https://sudden-death.onrender.com'}));
 
 mongoose.connect(process.env.MONGODB_URI)
 	.then(() => console.log('✅ Connected to MongoDB'))
