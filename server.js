d01e5791495d6f0119f556705440f45958417837
Kenny Allen <105110951+Lallen850@users.noreply.github.com>
2025-11-01 21:08:37 -0500

    Mobile

--- server.js
+++ server.js
@@ -11,7 +11,12 @@ const app = express();
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'dist')));
-app.use(cors({origin: 'https://sudden-death.onrender.com'}));
+app.use(cors({origin: true}));
+
+app.use((req, res, next) => {
+	console.log('Origin:', req.headers.origin);
+	next();
+});
 
 mongoose.connect(process.env.MONGODB_URI)
 	.then(() => console.log('✅ Connected to MongoDB'))
